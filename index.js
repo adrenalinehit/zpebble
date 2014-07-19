1ea11029fe15d10e33cfac4d87d80ae9afad3505
Nick Payne <nick.payne@controlf1.co.uk>
2014-07-19 21:58:27 +0100

    doesn't like require?

--- index.js
+++ index.js
@@ -67,7 +67,7 @@ simply.on('singleClick', function(e) {
 });
 
 populateData({
-	address: categories,
+	address: 'http://ec2-54-76-161-40.eu-west-1.compute.amazonaws.com/zanduli2/MobileService.svc/V2/en/AllCategory',
 	callback: popCats
 });
 
