7ee22304c6016867e77860d429b79dbf2b9bbb3c
Nick Payne <nick.payne@controlf1.co.uk>
2014-07-19 20:34:08 +0100

    try again....

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
-
-
-ajax({
-	url: 'http://ec2-54-76-161-40.eu-west-1.compute.amazonaws.com/zanduli2/MobileService.svc/V2/en/AllCategory'
-}, function(data) {
+ajax({url: 'http://ec2-54-76-161-40.eu-west-1.compute.amazonaws.com/zanduli2/MobileService.svc/V2/en/AllCategory'}, function(data) {
 	var cats = data.Data;
 
 	for (var i = 0; i < cats.length; i++) {
 		text += cats[i];
 	}
-		
-	simply.body(text);
 
+	simply.body(text);
 });
 
+simply.title('Testing....');
