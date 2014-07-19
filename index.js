d7696f0a960923a744427611fb9d46f881d39570
Nick Payne <nick.payne@controlf1.co.uk>
2014-07-19 20:56:41 +0100

    using ajax json

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ simply.style('small');
 
 var text = '';
 
-ajax({url: 'http://ec2-54-76-161-40.eu-west-1.compute.amazonaws.com/zanduli2/MobileService.svc/V2/en/AllCategory'}, function(data) {
+ajax({url: 'http://ec2-54-76-161-40.eu-west-1.compute.amazonaws.com/zanduli2/MobileService.svc/V2/en/AllCategory', type: 'json'}, function(data) {
 	var cats = data.Data;
 
 	for (var i = 0; i < cats.length; i++) {
